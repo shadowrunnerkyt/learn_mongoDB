8bd861d2aece7bd47e9e293294416d4c0e5a4308
shadowrunnerkyt <shadowrunnerkyt@users.noreply.github.com>
2019-07-27 23:10:46 -0400

    can list, add, update and delete categories

--- taskmanager/js/main.js
+++ taskmanager/js/main.js
@@ -5,13 +5,23 @@
  *******************************************/
 
 $(document).ready(function(){
+	// task events
 	$("#submitTask").click(addTask);
 	$("#updateTask").click(updateTask);
 	$("body").on('click','.btn-edit-task',setTask);
 	$("body").on('click','.btn-delete-task',deleteTask);
+	
+	// category events
+	$("#submitCategory").click(addCategory);
+	$("#updateCategory").click(updateCategory);
+	$("body").on('click','.btn-edit-cat',setCategory);
+	$("body").on('click','.btn-delete-cat',deleteCategory);
 });
 
- function getTasks(){
+// holds our JSON data
+var inputData;
+
+function getTasks(){
 	$.get('http://192.168.33.20:3001/taskmanager/tasks', function(data){
 		let output = '<ul class="list-group">';
 		$.each(data, function(key, task){
@@ -33,14 +43,15 @@ function addTask(event){
 	let category = $('#category').val();
 	let due_date = $('#due_date').val();
 	let is_urgent = ($('#is_urgent').val() == "true") ? true : false ;
+	inputData = JSON.stringify({
+		"task_name":task_name,
+		"category":category,
+		"due_date":due_date,
+		"is_urgent":is_urgent
+	});
 	$.ajax({
 		url:'http://192.168.33.20:3001/taskmanager/tasks',
-		data: JSON.stringify({
-			"task_name":  task_name,
-			"category": category,
-			"due_date": due_date,
-			"is_urgent": is_urgent
-		}),
+		data: inputData,
 		type: 'POST',
 		contentType: 'application/json',
 		success: goHome(),
@@ -49,7 +60,6 @@ function addTask(event){
 	event.preventDefault();
 }
 
-var inputData;
 function updateTask(event){
 	event.preventDefault();
 	let task_id = sessionStorage.getItem('current_id');
@@ -111,12 +121,84 @@ function setTask(){
 	return false
 }
 
+function setCategory(){
+	let cat_id = $(this).data('cat-id');
+	sessionStorage.setItem('current_id', cat_id);
+	window.location.replace("editcategory.html");
+	return false
+}
+
 function getTask(id){
 	$.get('http://192.168.33.20:3001/taskmanager/tasks/'+id, function(task){
-		// console.log('editing task: '+task.task_name);
+		console.log(task);
 		$('#task_name').val(task.task_name);
 		$('#category').val(task.category);
 		$('#due_date').val(task.due_date);
 		$('#is_urgent').val(String(task.is_urgent));
+	});
+}
+
+function getCategory(id){
+	$.get('http://192.168.33.20:3001/taskmanager/categories/'+id, function(cat){
+		console.log(cat);
+		$('#cat_name').val(cat.category_name);
+	});
+}
+
+function getCategories(){
+	$.get('http://192.168.33.20:3001/taskmanager/categories', function(data){
+		let output = '<ul class="list-group">';
+		$.each(data, function(key, cat){
+			output += '<li class="list-group-item">';
+			output += cat.category_name;
+			output += '<span class="float-right"><a class="btn btn-primary btn-sm btn-edit-cat" data-cat-name="'+cat.category_name+'" data-cat-id="'+cat._id+'" title="Edit:'+cat.category_name+'">Edit</a> <a class="btn btn-outline-danger btn-delete-cat btn-sm" data-cat-id="'+cat._id+'" title="Delete:'+cat.category_name+'">Delete</a></span>';
+		});
+		output += '</ul>';
+		$('#categories').html(output);
+	});
+}
+
+function addCategory(event){
+	let cat_name = $("#cat_name").val();
+	inputData = JSON.stringify({
+		"category_name": cat_name
+	});
+	$.ajax({
+		url:'http://192.168.33.20:3001/taskmanager/categories',
+		data: inputData,
+		type: 'POST',
+		contentType: 'application/json',
+		success: goHome(),
+		error: errAlert()
+	});
+	event.preventDefault();
+}
+
+function updateCategory(event){
+	event.preventDefault();
+	let cat_id = sessionStorage.getItem('current_id');
+	let cat_name = $('#cat_name').val();
+	inputData = JSON.stringify({
+		"category_name": cat_name
+	});
+	$.ajax({
+		url:"http://192.168.33.20:3001/taskmanager/categories/"+cat_id,
+		data: inputData,
+		method: "PUT",
+		contentType: 'application/json',
+		success: goHome(),
+		error: errAlert()
+	});
+}
+
+function deleteCategory(){
+	let cat_id = $(this).data('cat-id');
+	$.ajax({
+		url:"http://192.168.33.20:3001/taskmanager/categories/"+cat_id,
+		method: "DELETE",
+		contentType: 'application/json',
+		async: true,
+		success: goHome(),
+		error: errAlert()
 	});
 }
--- taskmanager/js/taskserver.js
+++ taskmanager/js/taskserver.js
@@ -33,20 +33,20 @@ app.get('/', (req, res, next) => {
 	res.send('Please use /taskmanager/tasks');
 });
 
-// Fetch all categories
-app.get('/taskmanager/categories', (req, res, next) => {
-	db.categories.find((err, docs) => {
-		if(err) {res.send(err);}
-		console.log('Categories Found...');
-		res.json(docs);
-	});
-});
-
 // Fetch all tasks
 app.get('/taskmanager/tasks', (req, res, next) => {
 	db.tasks.find((err, docs) => {
 		if(err) {res.send(err);}
 		console.log('Tasks Found...');
+		res.json(docs);
+	});
+});
+
+// Fetch all categories
+app.get('/taskmanager/categories', (req, res, next) => {
+	db.categories.find((err, docs) => {
+		if(err) {res.send(err);}
+		console.log('Categories Found...');
 		res.json(docs);
 	});
 });
@@ -60,10 +60,28 @@ app.get('/taskmanager/tasks/:id', (req, res, next) => {
 	});
 });
 
+// Fetch single category
+app.get('/taskmanager/categories/:id', (req, res, next) => {
+	console.log('Retrieving category: '+req.params.id);
+	db.categories.findOne({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+		if(err) {res.send(err);}
+		res.json(doc);
+	});
+});
+
 // Add task
 app.post('/taskmanager/tasks', (req, res, next) => {
 	console.log('Adding task... '+req.body.task_name);
 	db.tasks.insert(req.body), (err, doc) =>{
+		if(err){res.send(err);}
+		res.json(doc);
+	}
+});
+
+// Add category
+app.post('/taskmanager/categories', (req, res, next) => {
+	console.log('Adding category... '+req.body.category_name);
+	db.categories.insert(req.body), (err, doc) =>{
 		if(err){res.send(err);}
 		res.json(doc);
 	}
@@ -87,10 +105,36 @@ app.put('/taskmanager/tasks/:id', cors(), (req, res, next) => {
 		});
 });
 
+// Update a category
+app.put('/taskmanager/categories/:id', cors(), (req, res, next) => {
+	let cat_id = mongojs.ObjectId(req.params.id);
+	console.log('Updating task: '+cat_id);
+	db.categories.update(
+		{_id : cat_id},
+		{$set: {
+			category_name: req.body.category_name,
+		}}, 
+		(err, doc) => {
+			if(err){res.send(err);}
+			res.json(doc);
+		});
+});
+
 // Delete a task
 app.delete('/taskmanager/tasks/:id', (req, res, next) => {
 	console.log('Removing task: '+req.params.id);
 	db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+		if(err){
+			res.send(err);
+		}
+		res.json(doc);
+	});
+});
+
+// Delete a category
+app.delete('/taskmanager/categories/:id', (req, res, next) => {
+	console.log('Removing category: '+req.params.id);
+	db.categories.remove({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
 		if(err){
 			res.send(err);
 		}
